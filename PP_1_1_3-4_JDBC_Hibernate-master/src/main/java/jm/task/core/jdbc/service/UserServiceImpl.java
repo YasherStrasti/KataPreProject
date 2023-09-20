9e1d47e8c13a909d39a2fa7857074c0067ad1786
Vyacheslav <Commando6464@yandex.ru>
2023-09-20 12:49:35 +0300

    1.1.5

--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,42 +2,41 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private final UserDao userDaoJDBC = new UserDaoJDBCImpl();
+    private final UserDao userDao = new UserDaoHibernateImpl();
 
     public UserServiceImpl() {
 
     }
 
     public void createUsersTable() {
-        userDaoJDBC.createUsersTable();
+        userDao.createUsersTable();
         System.out.println("Table created!");
     }
 
     public void dropUsersTable() {
-        userDaoJDBC.dropUsersTable();
+        userDao.dropUsersTable();
         System.out.println("Table deleted!");
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        userDaoJDBC.saveUser(name, lastName, age);
+        userDao.saveUser(name, lastName, age);
         System.out.println("User saved!");
     }
 
     public void removeUserById(long id) {
-        userDaoJDBC.removeUserById(id);
+        userDao.removeUserById(id);
         System.out.println("User removed from table!");
 
     }
 
     public List<User> getAllUsers() {
-        List<User> userList = userDaoJDBC.getAllUsers();
+        List<User> userList = userDao.getAllUsers();
         for (User u : userList) {
             System.out.println(u.toString());
         }
@@ -45,7 +44,7 @@ public class UserServiceImpl implements UserService {
     }
 
     public void cleanUsersTable() {
-        userDaoJDBC.cleanUsersTable();
+        userDao.cleanUsersTable();
         System.out.println("Table cleared!");
     }
 }
